776cf22a67530351231cb2f51a799f90be2c5f2d
QuantScientist <shlomokashani@gmail.com>
2016-04-29 23:24:59 +0300

    Create MA1.R

---
+++ MA1.R
@@ -0,0 +1,29 @@
+//An MA(1) process in C++ & Rcpp. 
+//xt = 10 + wt + .7wt-1
+//
+//Invoke using: 
+//  y =(ma2(10,1500, 0.7, 1))
+//  mean(y)
+//  sd(y)
+//  acf(y,xlim=c(1,10))
+
+// [[Rcpp::export]]
+NumericVector ma2(double u, int n, double phi, double sigma){
+  std::random_device rd;
+  std::mt19937 e2(rd());
+  std::normal_distribution<> dist(0, sigma);
+  
+  NumericVector out(n);
+  //NumericVector wt(n);
+  double wt1;
+  double wt2;
+  //cores <- parallel::detectCores();
+  wt1=dist(e2);
+  wt2=dist(e2);
+  for(int i = 0; i < n; i++) {
+    out[i] = u + wt2+ phi*wt1;
+    wt1=wt2;
+    wt2=dist(e2);
+  }
+  return out;
+}
